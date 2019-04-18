5809ed9aab17b0d87bc965ca3fddd7c68f829323
Olga Isakova <penumbral@yandex.ru>
2019-04-18 20:48:12 +0300

    Apollo Boost -> Apollo Client

--- client/src/graphql.js
+++ client/src/graphql.js
@@ -1,9 +1,22 @@
-import { gql } from "apollo-boost";
+import gql from "graphql-tag";
 
 export const GET_CITIES = gql`
-  {
+  query getCities {
     cities {
       title
+      id
     }
+  }
+`;
+
+export const GET_JOBS = gql`
+  query getJobs($city: CityInput!) {
+    jobs(city: $city)
+  }
+`;
+
+export const SEND_EMAIL = gql`
+  mutation send($emailData: EmailInput!) {
+    send(emailData: $emailData)
   }
 `;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo-hooks";
 import * as serviceWorker from "./serviceWorker";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { ApolloLink } from "apollo-link";
 
 const client = new ApolloClient({
-  uri: "https://hello-tra.now.sh/graphql"
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: "https://hello-tra.penumbra1.now.sh/graphql",
+      credentials: "same-origin"
+    })
+  ]),
+  cache: new InMemoryCache({ addTypename: false })
 });
 
 ReactDOM.render(
