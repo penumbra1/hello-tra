4e96eebb7c21218687ce11870ca5e36c25eccf16
Olga Isakova <penumbral@yandex.ru>
2019-04-18 05:11:34 +0300

    Page header

---
+++ client/src/MenuButton.js
@@ -0,0 +1,82 @@
+import React from "react";
+import styled from "@emotion/styled/macro";
+// Adapted from Hamburgers by Jonathan Suh @jonsuh
+// https://github.com/jonsuh/hamburgers
+
+const Hamburger = props => (
+  <button type="button" {...props}>
+    <span>
+      <div />
+    </span>
+  </button>
+);
+
+const MenuButton = styled(Hamburger)`
+  --padding: 15px;
+  --layer-width: 28px;
+  --layer-height: 2px;
+  --layer-spacing: 6px;
+  --layer-offset: calc(var(--layer-spacing) + var(--layer-height));
+  --layer-color: ${props => props.theme.textColor};
+  --layer-border-radius: 4px;
+  --hover-opacity: 0.6;
+
+  padding: var(--padding);
+  display: inline-block;
+  cursor: pointer;
+  transition: opacity 0.15s linear;
+
+  font: inherit;
+  color: inherit;
+  text-transform: none;
+  background-color: transparent;
+  border: 0;
+  margin: 0;
+  overflow: visible;
+
+  &:hover,
+  &:active {
+    opacity: var(--hover-opacity);
+  }
+
+  span {
+    width: var(--layer-width);
+    height: calc(var(--layer-height) * 3 + var(--layer-spacing) * 2);
+    display: block;
+    position: relative;
+  }
+
+  div {
+    top: 50%;
+    margin-top: calc(var(--layer-height) / -2);
+
+    &,
+    &::before,
+    &::after {
+      width: var(--layer-width);
+      height: var(--layer-height);
+      background-color: var(--layer-color);
+      border-radius: var(--layer-border-radius);
+      position: absolute;
+    }
+
+    &::before,
+    &::after {
+      content: "";
+      display: block;
+      transition: transform 0.08s ease-out, opacity 0s 0.08s linear;
+    }
+
+    &::before {
+      transform: ${({ isOpen }) =>
+        `translateY(${isOpen ? 0 : "calc(var(--layer-offset) * -1)"} )`};
+    }
+
+    &::after {
+      transform: ${({ isOpen }) =>
+        `translateY(${isOpen ? 0 : "var(--layer-offset)"} )`};
+    }
+  }
+`;
+
+export default MenuButton;
--- client/src/PageHeader.js
+++ client/src/PageHeader.js
@@ -1,21 +1,33 @@
-import React from "react";
-import styled from "@emotion/styled";
-import { ReactComponent as Logo } from "./logo.svg";
-import { ReactComponent as Burger } from "./burger.svg";
+import React, { useState } from "react";
+import styled from "@emotion/styled/macro";
+import { ReactComponent as SvgLogo } from "./logo.svg";
+import MenuButton from "./MenuButton";
+
+const Logo = styled(SvgLogo)``;
 
 const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  ${Logo} {
+    margin: 72px auto;
+  }
+
+  ${MenuButton} {
+    position: fixed;
+    top: 0;
+    right: 0;
+    margin: 36px;
+  }
 `;
 
-const MenuButton = styled(Burger)``;
-
 const PageHeader = () => {
+  const [isMenuOpen, toggleMenu] = useState(false);
   return (
     <Header>
       <Logo />
-      <MenuButton />
+      <MenuButton isOpen={isMenuOpen} onClick={() => toggleMenu(!isMenuOpen)} />
     </Header>
   );
 };
